8c4ceb7cba975a85757ae760fcea584a3d900c29
Daniel Medeiros <danielmmf@gmail.com>
2017-12-17 16:19:55 -0200

    paginacao

--- resources/views/betlolo/ranking.php
+++ resources/views/betlolo/ranking.php
@@ -70,10 +70,10 @@
                                     </li>
                                     <li class="page-item active"><a class="page-link" href="#">1</a>
                                     </li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a>
+                                    <!--li class="page-item"><a class="page-link" href="#">2</a>
                                     </li>
                                     <li class="page-item"><a class="page-link" href="#">3</a>
-                                    </li>
+                                    </li-->
                                     <li class="page-item"><a class="page-link" href="#">Próximo</a>
                                     </li>
                                 </ul>
